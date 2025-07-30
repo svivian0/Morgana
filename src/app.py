623a10eced6a0582eb93e186abc1053d92916f30
Samuel <vivi4n.sam@gmail.com>
2025-07-30 04:07:36 -0300

    catar hora

--- src/app.py
+++ src/app.py
@@ -6,10 +6,22 @@ class Morgana:
     def __init__(self):
         pass
 
-    def hora(self):
-        agora=dt.datetime.now()
-        hora=agora.strftime("%H:%M")
-        print(f"A hora é: {hora}")
+    def catarDataHora(self):
+        self.datahora=dt.datetime.now()
 
+    def hora(self):
+        hora=self.datahora.strftime("%H:%M")
+        print(f"A hora é: {hora}")
+    
+    def diaSemana(self):
+        diasemana=self.datahora.strftime("%A")
+        print(f"Hoje é: {diasemana}")
+
+    def DiaMesAno(self):
+        dia=self.datahora.strftime("%d")
+        mes=self.datahora.strftime("%m")
+        ano=self.datahora.strftime("%Y")
+        print(f"Hoje é dia: {dia}, do mês: {mes}, do ano: {ano}")
 if __name__=="__main__":
     app=Morgana()
+    app.catarDataHora()
